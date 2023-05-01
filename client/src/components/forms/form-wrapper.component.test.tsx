e99705f710a1838d13b3d9934ce51c9714031486
Slawomir Dyk <slawomir.dyk@astrosat.net>
2023-05-01 09:40:20 +0100

    fix(frontend): add a simple form to the form-wrapper test

--- client/src/components/forms/form-wrapper.component.test.tsx
+++ client/src/components/forms/form-wrapper.component.test.tsx
@@ -1,20 +1,25 @@
 import { describe, expect, it } from 'vitest';
 import { FormWrapper } from './form-wrapper.component';
 import { render, screen } from '~/test/utils';
-import { LoginForm } from '~/accounts/authentication/login-form.component';
-
-let loginUser: () => void;
 
 describe('form-wrapper', () => {
-  beforeEach(() => {
-    loginUser = vi.fn();
-  });
-  it('should render form-wrapper on the page with login form', () => {
+  it('should render form-wrapper on the page with a form', () => {
     render(
       <FormWrapper>
-        <LoginForm loginUser={loginUser} />
+        <form aria-label="Add your details">
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" id="firstName" />
+          <label htmlFor="lastName">Last Name:</label>
+          <input type="text" id="lastName" />
+
+          <button type="submit">Submit Me</button>
+        </form>
       </FormWrapper>,
     );
     expect(screen.getByRole('article')).toBeInTheDocument();
+    expect(screen.getByRole('form')).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: 'First Name:' })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: 'Last Name:' })).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
 });
