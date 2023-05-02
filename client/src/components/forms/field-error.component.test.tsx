2f0bacb7ca9dd84f8c76d79b1c4506f0450244ae
Slawomir Dyk <slawomir.dyk@astrosat.net>
2023-05-02 12:09:18 +0100

    fix(frontend): fix the field-error component tests as requested. I added ReactElement type to the children in the FieldErrorProps interface to fix the type script error.

--- client/src/components/forms/field-error.component.test.tsx
+++ client/src/components/forms/field-error.component.test.tsx
@@ -3,12 +3,20 @@ import { FieldError } from './field-error.component';
 import { render, screen } from '~/test/utils';
 
 describe('form-error', () => {
-  it('should render field-error on the page', () => {
+  it('should render field-error on the page with text as children', () => {
     render(<FieldError>Test Error</FieldError>);
     expect(screen.getByText('Test Error')).toBeInTheDocument();
   });
-  it('should render field-error with proper font size and message colour', () => {
-    render(<FieldError>Test Error</FieldError>);
-    expect(screen.getByText('Test Error')).toHaveClass('text-sm text-red-600');
+  it('should render field-error on the page with some HTML as children', () => {
+    render(
+      <FieldError>
+        <div>
+          <span>Test</span>
+          <span>Error</span>
+        </div>
+      </FieldError>,
+    );
+    expect(screen.getByText('Test')).toBeInTheDocument();
+    expect(screen.getByText('Error')).toBeInTheDocument();
   });
 });
